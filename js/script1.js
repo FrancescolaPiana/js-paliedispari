0df52eab0b0b3b64e2d1e926b849c1643cae2c1f
Francescolapiana <Lushika1219@gmail.com>
2022-10-19 21:52:48 +0200

    FINE

--- js/script1.js
+++ js/script1.js
@@ -1,3 +1,29 @@
 // Palidroma
 // Chiedere all’utente di inserire una parola
 // Creare una funzione per capire se la parola inserita è palindroma
+
+
+let res;
+let btn = document.getElementById('btn');
+btn.addEventListener('click' , fx)
+
+function fx() {
+    let arraybase = [];
+    let cell = document.getElementById('cell').value
+    console.log(cell); 
+
+    let split = cell.split('').reverse().join('')
+
+
+    if (cell == split) {
+        res = document.getElementById('response').innerHTML = 'è un palindromo';
+        res.innerHTML = `L'input è Palindromo`
+    }
+    else{
+        res = document.getElementById('response').innerHTML = 'Non è un palindromo';
+        res.innerHTML = `L'input NON è Palindromo`
+    }
+
+    console.log('array iniziale' , split);
+    console.log('array iniziale' , cell);
+}
