24a94c933e4d9217e680c0812cf52bc53c362803
Francescolapiana <Lushika1219@gmail.com>
2022-10-24 10:05:27 +0200

    ended

--- js/script2.js
+++ js/script2.js
@@ -5,3 +5,87 @@
 // Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 // Dichiariamo chi ha vinto.
 
+function randomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
+
+// let numrandom = randomNumber(1, 5);
+// console.log(numrandom);
+
+
+// FX PARI
+let btnpari = document.getElementById('pari')
+btnpari.addEventListener('click', pari )
+
+function pari() {
+    let numrandom = randomNumber(1, 5);
+    console.log('numero randomizzato =' ,numrandom);
+
+    let inputhuman = document.getElementById('number').value
+    let number = parseInt(inputhuman);
+    console.log('numero del player =' , number); 
+    let result = numrandom + number;
+    if (number > 5) {
+        document.getElementById('response').innerHTML = 
+        '<span class="errors">Puoi inserire numeri solo compresi tra 1 e 5</span>'
+        document.getElementById('number').value = ''
+    }
+    else if(number < 1){
+        document.getElementById('response').innerHTML = 
+        '<span class="errors">Puoi inserire numeri solo compresi tra 1 e 5</span>'
+        document.getElementById('number').value = ''
+    }
+    else if(isNaN(number)){
+        document.getElementById('response').innerHTML = 
+        '<span class="errors">Puoi inserire numeri solo compresi tra 1 e 5</span>'
+        document.getElementById('number').value = ''
+    }
+    else{
+        if ((result % 2) == 0) {
+            document.getElementById('response').innerHTML = 'Hai Vinto'
+        }
+        else{
+            document.getElementById('response').innerHTML = 'Hai Perso'
+        }
+        document.getElementById('number').value = ''
+    }
+   }
+
+   
+// FX DISPARI
+let btndispari = document.getElementById('dispari')
+btndispari.addEventListener('click', dispari )
+
+function dispari() {
+    let numrandom = randomNumber(1, 5);
+    console.log('numero randomizzato =' ,numrandom);
+
+    let inputhuman = document.getElementById('number').value
+    let number = parseInt(inputhuman);
+    console.log('numero del player =' , number); 
+    let result = numrandom + number;
+    if (number > 5) {
+        document.getElementById('response').innerHTML = 
+        '<span class="errors">Puoi inserire numeri solo compresi tra 1 e 5</span>'
+        document.getElementById('number').value = ''
+    }
+    else if(number < 1){
+        document.getElementById('response').innerHTML = 
+        '<span class="errors">Puoi inserire numeri solo compresi tra 1 e 5</span>'
+        document.getElementById('number').value = ''
+    }
+    else if(isNaN(number)){
+        document.getElementById('response').innerHTML = 
+        '<span class="errors">Puoi inserire numeri solo compresi tra 1 e 5</span>'
+        document.getElementById('number').value = ''
+    }
+    else{
+        if ((result % 2) == 0) {
+            document.getElementById('response').innerHTML = 'Hai Perso'
+        }
+        else{
+            document.getElementById('response').innerHTML = 'Hai Vinto'
+        }
+        document.getElementById('number').value = ''
+    }
+   }
